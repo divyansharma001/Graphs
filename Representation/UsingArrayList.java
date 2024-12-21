0ae198fa840738699e59845f63ee63918b03a8e2
Divyansh Sharma <divyanshsharma4507@gmail.com>
2024-12-21 18:51:07 +0530

    Fix indexing in UsingArrayList class for adjacency list representation

--- Representation/UsingArrayList.java
+++ Representation/UsingArrayList.java
@@ -9,7 +9,7 @@ public class UsingArrayList {
         ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>();
 
         // for 1 based indexing otherwise <n
-        for (int i = 0; i <= r; i++) {
+        for (int i = 0; i <= n; i++) {
             adj.add(new ArrayList<>());
         }
 
@@ -34,12 +34,12 @@ public class UsingArrayList {
         adj.get(3).add(1);
         adj.get(1).add(3);
 
-        printArrayList(r, c, adj);
+        printArrayList(n, m, adj);
 
     }
 
-    private static void printArrayList(int r, int c, ArrayList<ArrayList<Integer>> adj) {
-        for (int i = 0; i <= r; i++) {
+    private static void printArrayList(int n, int m, ArrayList<ArrayList<Integer>> adj) {
+        for (int i = 0; i <= n; i++) {
             for (int j = 0; j < adj.get(i).size(); j++) {
                 System.out.print(adj.get(i).get(j) + " ");
             }
