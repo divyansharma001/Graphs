6b3bf68667fdf23528745be958e9d90a2681c7a0
Divyansh Sharma <divyanshsharma4507@gmail.com>
2024-12-22 14:26:08 +0530

    Add BFS class with algorithm explanation and queue behavior notes

---
+++ Traversal/BFS.java
@@ -0,0 +1,38 @@
+package Traversal;
+
+public class BFS {
+
+}
+
+/*
+ * Algorithm:
+ * 
+ * 1. Queue data structure: follows FIFO, and will always contain the starting.
+ * 2. Visited array: an array initialized to 0
+ * 3. In BFS, we start with a “starting” node, mark it as visited, and push it
+ * into the queue data structure.
+ * 4. In every iteration, we pop out the node ‘v’ and put it in the solution
+ * vector, as we are traversing this node.
+ * 5. All the unvisited adjacent nodes from ‘v’ are visited next and are pushed
+ * into the queue. The list of adjacent neighbors of the node can be accessed
+ * from the adjacency list.
+ * 6. Repeat steps 2 and 3 until the queue becomes empty, and this way you can
+ * easily traverse all the nodes in the graph.
+ * 
+ */
+
+/*
+ * Diffrence between .poll() and .remove() in queue in JAVA
+ 1. poll()
+ * Description: Retrieves and removes the head of the queue, or returns null if
+   the queue is empty.
+ * Behavior with Empty Queue: Returns null instead of throwing an exception.
+ * Use Case: When you want to handle an empty queue gracefully without dealing
+   with exceptions.
+ 2. remove()
+ * Description: Retrieves and removes the head of the queue, but throws a
+   NoSuchElementException if the queue is empty.
+ * Behavior with Empty Queue: Throws a NoSuchElementException.
+ * Use Case: When you are certain the queue is not empty and want to enforce
+   stricter error handling.
+ */
