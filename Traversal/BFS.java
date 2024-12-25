fac7be0d19108c6bf810ce057206b181c475fb2f
Divyansh Sharma <divyanshsharma4507@gmail.com>
2024-12-25 13:08:38 +0530

    Implement BFS algorithm with graph traversal method

--- Traversal/BFS.java
+++ Traversal/BFS.java
@@ -1,6 +1,33 @@
 package Traversal;
 
+import java.util.*;
+
 public class BFS {
+
+public ArrayList<Integer> bfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj){
+ArrayList<Integer> bfs = new ArrayList<>();
+boolean[] visited = new boolean[V];
+Queue<Integer> q = new LinkedList<>();
+
+q.add(0);
+visited[0]=true;
+
+while(!q.isEmpty()){
+  Integer node = q.poll();
+  bfs.add(node);
+
+  for (int i = 0; i < adj.get(node).size(); i++) {
+    int it = adj.get(node).get(i);
+    if (!visited[it]) {
+        visited[it] = true;
+        q.add(it);
+    }
+}
+
+}
+
+return bfs;
+}
 
 }
 
